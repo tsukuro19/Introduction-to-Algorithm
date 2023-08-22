8d5e0f92d0a176a54a4d0db1514cf85facb83fc2
Tsukuro <vongola392112@gmail.com>
2023-08-22 18:28:36 +0700

    change content file

--- lesson 2/Insertion sort/Insertion sort.py
+++ lesson 2/Insertion sort/Insertion sort.py
@@ -1,4 +1,4 @@
-def Insertion_sort(array):
+def Insertion_Sort(array):
     for i in range(1,len(array)):
         key=array[i]
         j=i-1
@@ -8,6 +8,5 @@ def Insertion_sort(array):
         array[j+1]=key
     return array
 
-if __name__=="__main__":
-    array_test=list(map(int,input().split()))
-    print(Insertion_sort(array_test))
+array_test=[5,2,4,6,1,3]
+print(Insertion_Sort(array_test))
